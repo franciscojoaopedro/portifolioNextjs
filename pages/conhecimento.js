5f8093035d338ee3b30e6c6a97446ab4e50cff19
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-09-26 11:40:48 +0100

    first commit

---
+++ pages/conhecimento.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+const Skills=()=>{
+    return(
+        <>
+        <Head>
+            <title>Conhecimentos</title>
+        </Head>
+        <h1>Meus Conhecimentos</h1>
+        </>
+    )
+}
+export default Skills;
---
+++ pages/experiencias.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+const Experiencia=()=>{
+    return(
+        <>
+        <Head>
+            <title>Experiencias</title>
+        </Head>
+        <h1>Experiencias</h1>
+        </>
+    )
+}
+
+export default Experiencia;
